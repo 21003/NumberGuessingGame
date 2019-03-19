066592bc183de8b5b9f94345a8f4b3d21e91f344
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-03-19 13:47:11 -0400

    Started a random number generator

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -4,7 +4,13 @@ public class NumberGame {
 
     public static void main(String[] args) {
 
-       
+        int randomNumber, userNumber;
+
+        randomNumber = (int)(Math.random()*100);
+
+        System.out.println("Hello this is a number guessing game can you give me a number to start");
+
+        System.out.println(randomNumber);
 
         
 
